941ad3d94fffd2320eb3f96ef2c3d74b85f6f5ba
KrishSavaj <savajkrish98@gmail.com>
2024-04-05 13:51:46 +0530

    ignore

--- controllers/login.js
+++ controllers/login.js
@@ -6,13 +6,11 @@ const Login = require("../models/login");
 const jwt = require("jsonwebtoken");
 //
 
-const { JWT_SECRET } = require("../config");
-
 module.exports.authentication = async (req, res) => {
   const success = await Login.findOne(req.body);
 
   if (success) {
-    const token = jwt.sign({ userId: success._id }, JWT_SECRET);
+    const token = jwt.sign({ userId: success._id }, process.env.JWT_SECRET);
 
     res.json(token);
     return;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 //
 
+require("dotenv").config();
+
 const cors = require("cors");
 app.use(cors());
 
